ff48f301500cff2ac6d3e14b79fa55690ad95b2d
zohaib ismail <Clamboy076@gmail.com>
2024-08-13 12:43:44 +0500

    Title Size Changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,9 @@ import SizeChart from './components/SizeChartPage';
 const RingSizeCalculator = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-4 flex flex-col items-center justify-start">
-      <div className="flex items-center justify-between w-full max-w-md mb-8">
-        <h1 className="text-3xl md:text-4xl font-bold text-gray-800">RING SIZE CALCULATOR</h1>
+    <div className="w-full max-w-md mb-8">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800 text-center">
+          RING SIZE CALCULATOR</h1>
       </div>
 
       <SizeChart />
